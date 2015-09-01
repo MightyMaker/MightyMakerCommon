119a98c8bf6d04cff6808b4ef538308773d9f454
Riaan Pelser <riaanpelser@gmx.com>
2015-09-01 22:23:15 +0200

    Added gulpfile.js with SASS compilation (gulp, gulp-sass)

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+var paths =
+{
+    sass_files: "styles/sass/**/*.scss",
+    css: "styles/css"
+};
+
+gulp.task("sass", function()
+{
+    return gulp.src(paths.sass_files)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest(paths.css))
+});
+
+gulp.task("sass:watch", function()
+{
+    gulp.watch(paths.sass_files, ["sass"]);
+});
+
+gulp.task("default", ["sass:watch", "sass"]);
